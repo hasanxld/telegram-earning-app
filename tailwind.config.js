b18c59ad0f479fef0f6b2651dab7d67099eac715
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-14 16:38:45 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,18 +19,6 @@ module.exports = {
           600: '#2563eb',
           700: '#1d4ed8',
           900: '#1e3a8a',
-        },
-        success: {
-          500: '#22c55e',
-          600: '#16a34a',
-        },
-        warning: {
-          500: '#eab308',
-          600: '#ca8a04',
-        },
-        danger: {
-          500: '#ef4444',
-          600: '#dc2626',
         }
       },
       borderRadius: {
