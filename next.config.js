d3457a2e5594c9d07ab17ac39971989805d1913a
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-14 16:39:14 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['localhost'],
-  },
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
 }
 
