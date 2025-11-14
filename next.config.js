06bf426c8a9b4eb7a9f10774a6661309c1b2515c
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-14 16:25:15 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
+}
+
+module.exports = nextConfig
