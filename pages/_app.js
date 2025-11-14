231276ff79275f924431163bd2793091b68c1244
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-14 16:41:06 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,12 @@
 // pages/_app.js
 import '../styles/globals.css'
-import { useState, useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  const [telegramReady, setTelegramReady] = useState(false)
-
-  useEffect(() => {
-    // Load Telegram Web App script
-    const script = document.createElement('script')
-    script.src = 'https://telegram.org/js/telegram-web-app.js'
-    script.async = true
-    script.onload = () => setTelegramReady(true)
-    document.head.appendChild(script)
-
-    return () => {
-      document.head.removeChild(script)
-    }
-  }, [])
-
   return (
     <>
-      <Script
-        src="https://telegram.org/js/telegram-web-app.js"
-        strategy="beforeInteractive"
-      />
       <Toaster position="top-right" />
-      <Component {...pageProps} telegramReady={telegramReady} />
+      <Component {...pageProps} />
     </>
   )
 }
