4d177a276bc26c0f5b6bf0510df5ebe99144d444
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-14 16:50:56 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 // pages/_app.js
 import '../styles/globals.css'
 import { Toaster } from 'react-hot-toast'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Load Telegram Web App script
+    const script = document.createElement('script')
+    script.src = 'https://telegram.org/js/telegram-web-app.js'
+    script.async = true
+    document.head.appendChild(script)
+
+    return () => {
+      document.head.removeChild(script)
+    }
+  }, [])
+
   return (
     <>
       <Toaster position="top-right" />
